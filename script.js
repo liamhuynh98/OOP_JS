084962ff9cd77c72ebf258d446622dd548425bea
liamhuynh98 <liamhuynh21@gmail.com>
2022-04-05 17:37:50 -0500

    Learned about prototypes and prototypeOfLinkedObject

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ const Person = function (firstName, birthYear) {
 const liam = new Person('Liam', 1998);
 const kevin = new Person('Kevin', 2000);
 console.log(liam, kevin);
+
+//prototype
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+liam.calcAge();
+kevin.calcAge();
+
+console.log(liam.__proto__);
+console.log(kevin.__proto__);
+
+console.log(Person.prototype.isPrototypeOf(liam));
+console.log(Person.prototype.isPrototypeOf(kevin));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+//.prototypeOfLinkedObject
+Person.prototype.species = 'Straight';
+console.log(liam.species);
+
+//checking for properties of the object
+console.log(liam.hasOwnProperty('firstName'));
+console.log(liam.hasOwnProperty('species'));
