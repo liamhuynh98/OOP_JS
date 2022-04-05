ae50a8dd5b620b7a39f2df383b2eb7d3d71cdcaa
liamhuynh98 <liamhuynh21@gmail.com>
2022-04-05 16:35:52 -0500

    Learned about JS constructor & the new operator

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+//constructor function naming start with a capital letter
+//creating a person
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const liam = new Person('Liam', 1998);
+const kevin = new Person('Kevin', 2000);
+console.log(liam, kevin);
