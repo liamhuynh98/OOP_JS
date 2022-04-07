33fd65fed3fb70ab6d6cf899f76f51232e120410
liamhuynh98 <liamhuynh21@gmail.com>
2022-04-07 18:42:49 -0500

    Learned Getter and Setter

--- script.js
+++ script.js
@@ -89,11 +89,27 @@ class PersonCl {
   calcAge() {
     console.log(2037 - this.birthYear);
   }
+
+  //getter
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes('')) this._fullName; = name; 
+    else alert(`${name} is not a full name`)
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
 const jessica = new PersonCl('Jessica', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.__proto__);
 PersonCl.prototype.greet = function () {
@@ -101,3 +117,21 @@ PersonCl.prototype.greet = function () {
 };
 
 jessica.greet();
+
+//Setters and Setters
+const account = {
+  owner: 'Liam',
+  movements: [300, 220, 111, 888],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
+console.log(accoun.movements);
