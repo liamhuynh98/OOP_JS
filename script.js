764b03b632e8876a191e927bcdbc3b39c5631bda
liamhuynh98 <liamhuynh21@gmail.com>
2022-04-06 20:03:13 -0500

    Created A Car Object with 2 prototypes

--- script.js
+++ script.js
@@ -35,3 +35,41 @@ console.log(liam.species);
 //checking for properties of the object
 console.log(liam.hasOwnProperty('firstName'));
 console.log(liam.hasOwnProperty('species'));
+
+//Prototypal inheritance on built-in Objects
+console.dir(Person.prototype.constructor);
+const arr = [3, 6, 4, 6, 7, 2, 3, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique);
+const h1 = document.querySelector('h1');
+
+//Coding Challenge 1
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10; 
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+}
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log{`${this.make} is going at ${this.speed} km/h`};
+
+}
+
+const bwm = new Car ('BWM', 120);
+const mercedes = new Car ('Mercedes', 95);
+
+bwm.accelerate();
+bwm.accelerate();
+bwm.brake();
