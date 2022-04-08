9b7ccb1e37c5dc0a77b7a6e0798a7259613e291b
liamhuynh98 <liamhuynh21@gmail.com>
2022-04-07 22:47:21 -0500

    Inheritance between classes: constructor functions

--- script.js
+++ script.js
@@ -149,3 +149,67 @@ const steven = Object.create(PersonProto);
 console.log(steven);
 steven.birthYear = 2002; 
 steven.calcAge();
+
+//coding challenge 
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed =  speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} mi/h`)
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at this ${this.speed} mi/h`)
+  }
+
+  get speedUS() {
+    return this.speed / 1.6; 
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6
+  }
+}
+
+const ford = new CarCl('Ford', 120)
+console.log(ford.speedUS)
+ford.accelerate(); 
+ford.brake();
+ford.speedUS = 50;
+console.log(ford)
+
+//Inheritance between "classes": constructor functions 
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear)
+  this.course = course;
+}
+//linking prototype
+Student.prototype = Object.create(Person.prototype)
+
+Student.prototype.introduce = function() {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+}
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+mike.calcAge();
+console.log(mike.__proto__)
+console.log(mike.__proto__.prototype)
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+Student.prototype.constructor = Student; 
+console.dir(Student.prototype.constructor);
