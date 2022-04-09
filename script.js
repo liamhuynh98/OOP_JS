7c82f66769c858933e3d72d59ae0de1b1ed0ffd3
liamhuynh98 <liamhuynh21@gmail.com>
2022-04-09 15:54:13 -0500

    Code Challenge

--- script.js
+++ script.js
@@ -213,3 +213,43 @@ console.log(mike instanceof Student);
 console.log(mike instanceof Person);
 Student.prototype.constructor = Student; 
 console.dir(Student.prototype.constructor);
+
+//challenge #2 
+const Car = function(make, speed) {
+  this.make = make; 
+  this.speed = speed;
+}
+
+Car.prototype.accelerate = function () {
+  this.speed += 10; 
+  console.log(`${this.make} is going at ${this.speed} km/h`)
+}
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is goingf at ${this.speed} km/h`)
+}
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+}
+//link the prototype of Cars to EV 
+
+EV.prototype = Create.Object(Car.prototype);
+EV.prototype.chargeBattery = function(chargeTo) {
+  this.charge = chargeTo;
+}
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(`${this.make} is going at ${this.speed} km/h with a charge of ${this.charge}`)
+}
+
+const tesla = new EV('Tesla', 120, 23)
+tesla.chargeBattery(90)
+console.log(tesla);
+tesla.brake();
+tesla.accelerate();
+
+//inheritance between classes in ES6 
