70f6c76b3161da06f2a694e32f2ca5738e71f53c
liamhuynh98 <liamhuynh21@gmail.com>
2022-04-10 15:53:04 -0700

    Private classes

--- script.js
+++ script.js
@@ -309,22 +309,28 @@ jay.calcAge();
 
 //Another example of classes
 class Account {
+  //public field
+  locale = navigator.language;
+
+  //private field
+  #movements = [];
+  #pin;
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    //this._movements = [];
+    //this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposite(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
