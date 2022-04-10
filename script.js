f3454a04f07256e3c9d29478aad40e6bc374a7f8
liamhuynh98 <liamhuynh21@gmail.com>
2022-04-10 10:48:05 -0700

    Protected properties

--- script.js
+++ script.js
@@ -308,3 +308,43 @@ jay.introduce();
 jay.calcAge();
 
 //Another example of classes
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this._movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  getMovements() {
+    return this._movements;
+  }
+
+  deposite(val) {
+    this._movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposite(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposite(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Liam', 'USD', 1111);
+acc1.deposite(250);
+acc1.withdraw(150);
+console.log(acc1);
+console.log(getMovements());
